f02253f10dab74f8e5d5521e55e7f675c6969044
dodoria721 <126737446+dodoria721@users.noreply.github.com>
2023-05-16 12:57:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(np.random.randn(500,2)/[50,50] + [37.76-122.4],columns=['lat','lon'])
+df = pd.DataFrame(np.random.randn(500,2)/[50,50] + [37.76,-122.4],columns=['lat','lon'])
 st.map(df)
 
 rand = np.random.normal(1, 2, size=20)
